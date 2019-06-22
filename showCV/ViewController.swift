cb0852598a6c919f807d6764f1b03038acc04e19
JATZ BH <jatziry.barrera.contractor@bbva.com>
2019-06-21 21:36:08 -0500

    Init project

---
+++ showCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  showCV
+//
+//  Created by Jatziry Barrera on 6/20/19.
+//  Copyright © 2019 Jatziry Barrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
